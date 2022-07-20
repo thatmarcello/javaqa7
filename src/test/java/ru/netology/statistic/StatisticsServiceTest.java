04a2e0f784d3905a5d959637937bde9f50819415
Mark Iarantsev <markyarantsev@yahoo.com>
2022-07-21 00:22:19 +0300

    second fix

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findEquals() {
+    void findMaxIfNot() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 150, 3, 8, 6, 11, 11, 12};
